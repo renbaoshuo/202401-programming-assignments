fa4d1064cd494ea2f796ec99451360e267ebf2ce
Baoshuo Ren <i@baoshuo.ren>
2024-10-16 11:29:14 +0800

    【实践课内】1.顺序结构

--- 【实践课内】1.顺序结构/7-4 21-爬楼梯.c
+++ 【实践课内】1.顺序结构/7-4 21-爬楼梯.c
@@ -5,7 +5,7 @@ int a, t, b;
 int main() {
     scanf("%d%d%d", &a, &t, &b);
 
-    printf("%d", 1ll * t * (b - 1) / (a - 1));
+    printf("%d", (long long)t * (b - 1) / (a - 1));
 
     return 0;
 }
